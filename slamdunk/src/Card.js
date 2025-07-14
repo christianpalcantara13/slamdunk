6b2b170d91867f43f57e370208ad07050d4e5c5b
Christian Alcantara <christianpalcantara13@gmail.com>
2025-07-15 00:54:47 +0800

    still coding

---
+++ slamdunk/src/Card.js
@@ -0,0 +1,16 @@
+//import React from 'react';
+
+const Card =()=>{
+    return(
+    <div>
+        <img alt ='slamdunk'src = ''/>
+        <div>
+            <h2> christian </h2>
+            <p>sample@aol.com</p>
+        </div>
+    </div>
+       
+    );
+
+}
+export default Card;
--- slamdunk/src/index.js
+++ slamdunk/src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from './Card';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-reportWebVitals();
-//
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<Card/>);
+
+
+
