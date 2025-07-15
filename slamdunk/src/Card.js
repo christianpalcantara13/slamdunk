1515b349282615556e178f42df81cb2a43ac16cf
Christian Alcantara <christianpalcantara13@gmail.com>
2025-07-15 14:45:00 +0800

    Insert Akagi.jpg

--- slamdunk/src/Card.js
+++ slamdunk/src/Card.js
@@ -1,9 +1,11 @@
-//import React from 'react';
+import React from 'react';
+import slamdunk from './Assets/Akagi.jpg';
 
 const Card =()=>{
     return(
     <div>
-        <img alt ='slamdunk'src = ''/>
+        <img src={slamdunk} alt="slamdunk" />;
+        <img alt ='slamdunk'src ='/Assets/Akagi.jpg'/>
         <div>
             <h2> christian </h2>
             <p>sample@aol.com</p>
@@ -11,6 +13,5 @@ const Card =()=>{
     </div>
        
     );
-
 }
 export default Card;
